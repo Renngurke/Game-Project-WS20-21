a0e0a67ae3e653b82f99f180326e69d297c0a621
Alexander Jodlauk <alexander.jodlauk@alumni.fh-aachen.de>
2020-10-30 22:26:31 +0100

    Basics
    Player and Camera Movement, Points to Mouse

---
+++ Game Project/Assets/Alex-Protoype/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float yOffset = 25;
+
+    [SerializeField] Transform follow;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(follow.position.x, follow.position.y + yOffset, follow.position.z);
+    }
+}
---
+++ Game Project/Assets/Alex-Protoype/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] Camera cam;
+
+    [SerializeField] float movementSpeed = 10f;
+
+    Vector3 movement = new Vector3(0f, 0f, 0f);
+    Vector3 mousePos;
+
+    private void Update()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        
+        mousePos = Input.mousePosition;
+    }
+    
+    void FixedUpdate()
+    {
+        controller.Move(movement * movementSpeed * Time.fixedDeltaTime);
+
+        Vector3 lookDir = mousePos - cam.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
+    }
+}
